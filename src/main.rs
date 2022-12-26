265d36c6a59b8346256de2c593c262bee040f583
Ukpeh Michael <marcelukpeh@gmail.com>
2022-12-27 00:00:27 +0100

    Database Setup Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Tcp Listener
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener)?.await
 }
